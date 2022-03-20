966c975c34e19a17a8379cb38e3555ac4f1146d9
iychoi <iychoi98@directionsoft.com>
2022-03-20 23:44:17 +0900

    프로젝트 세팅 완료

---
+++ wkwebkit_std/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wkwebkit_std
+//
+//  Created by iychoi on 2022/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
