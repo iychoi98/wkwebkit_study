d0f945ea7644138495455e2a802e22cb49315d4d
iychoi <iychoi98@directionsoft.com>
2022-03-21 02:28:02 +0900

    웹 하단 메뉴들 rx적용

--- wkwebkit_std/ViewControllers/WebViewController.swift
+++ wkwebkit_std/ViewControllers/WebViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 import WebKit
+import RxSwift
+import RxCocoa
+import RxGesture
 
 class WebViewController: UIViewController {
     @IBOutlet weak var wkWebView: WKWebView!
     
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initWebView()
+        bindAction()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,5 +44,29 @@ extension WebViewController: WKUIDelegate, WKNavigationDelegate {
         let request = URLRequest(url: url)
         
         wkWebView.load(request)
+    }
+}
+
+extension WebViewController {
+    private func bindAction() {
+        backButton.rx.tap
+            .bind {
+                print("backBtn")
+            }.disposed(by: disposeBag)
+        
+        forwardButton.rx.tap
+            .bind {
+                print("forwardBtn")
+            }.disposed(by: disposeBag)
+        
+        homeButton.rx.tap
+            .bind {
+                print("homeBtn")
+            }.disposed(by: disposeBag)
+        
+        resetButton.rx.tap
+            .bind {
+                print("resetBtn")
+            }.disposed(by: disposeBag)
     }
 }
