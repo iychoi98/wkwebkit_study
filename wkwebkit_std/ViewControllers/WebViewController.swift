e7bb0b38ec364676b822ed0a507115e050793da1
iychoi <iychoi98@directionsoft.com>
2022-03-20 23:56:37 +0900

    Main to Web 뷰 컨트롤러 이동 버튼 생성

---
+++ wkwebkit_std/ViewControllers/WebViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewController.swift
+//  wkwebkit_std
+//
+//  Created by iychoi on 2022/03/20.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
