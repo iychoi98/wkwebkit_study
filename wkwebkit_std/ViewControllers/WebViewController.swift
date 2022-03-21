ba75517b1714690a04ad6551e807ca6a59b96426
iychoi <iychoi98@directionsoft.com>
2022-03-21 12:13:10 +0900

    브라우저 경고창 처리

--- wkwebkit_std/ViewControllers/WebViewController.swift
+++ wkwebkit_std/ViewControllers/WebViewController.swift
@@ -32,6 +32,7 @@ class WebViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         loadUrl()
+        checkNetwork()
     }
 }
 
@@ -46,6 +47,18 @@ extension WebViewController {
         let request = URLRequest(url: url)
         
         wkWebView.load(request)
+    }
+    
+    func checkNetwork() {
+        guard Reachability.networkConnected() else {
+            let alertController = UIAlertController(title: "네트워크 오류", message: "네트워크 연결 확인 부탁드립니다.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "종료", style: .default) { (action) in
+                exit(0) //프로그램 종료 -> 메인화면으로 이동
+            }
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
     }
     
     private func backPage() {
@@ -120,19 +133,41 @@ extension WebViewController: WKUIDelegate {
         popupWebView = nil
     }
     
+    //MARK: - 브라우저 경고창 처리 (alert, confirm, prompt)
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertContoller = UIAlertController(title: "alertPanel", message: "messege", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "", style: .default) { (action) in
+            completionHandler()
+        }
+        alertContoller.addAction(alertAction)
+        self.present(alertContoller, animated: true, completion: nil)
+    }
     
-//
-//    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async {
-//        <#code#>
-//    }
-//
-//    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async -> Bool {
-//        <#code#>
-//    }
-//
-//    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo) async -> String? {
-//        <#code#>
-//    }
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertContoller = UIAlertController(title: "alertPanel", message: "messege", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default) { (action) in
+            completionHandler(true)
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .default) { (action) in
+            completionHandler(false)
+        }
+        alertContoller.addAction(confirmAction)
+        alertContoller.addAction(cancelAction)
+        self.present(alertContoller, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: "", message: prompt, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "확인", style: .default) { (action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            } else {
+                completionHandler(defaultText)
+            }
+        }
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
 //MARK: - WKNavigationDelegate 관련 함수
